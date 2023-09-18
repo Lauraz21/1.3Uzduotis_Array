fad624cde2a6cf06e3a1c79420968a6c4dadf01e
Laura <Laura@DESKTOP-BESSB6E>
2023-09-18 22:26:39 +0300

    Add project files.

---
+++ 1.3Uzduotis_Array/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _1._3Uzduotis_Array
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] randomNumbers = { 3, 9, 5, 3, 7 };
+
+            Console.WriteLine(MaxValue(randomNumbers));
+
+        }
+        public static int MaxValue(int[] randomNumbers)
+        {
+            int max = randomNumbers[0];
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                if (randomNumbers[i] > max)
+                {
+                    max = randomNumbers[i];
+                    
+                }
+            }
+            return max;
+        }
+    }
+}
